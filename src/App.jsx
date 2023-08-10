a80a9efbf1a5ae143d57a25c5b6ce12806842f42
Shivendra TechSter <shivendratechster@gmail.com>
2023-08-10 18:49:18 +0530

    shows tasks in the browser

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'Feb 5th at 2:30pm',
+      reminder: false,
+    }
+  ])
   return (
     <div className="Container">
       <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
